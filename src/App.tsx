53245ff41497e7da39cf361adacb32cf8e53ffd9
Bharat <bharat.rajoriya11@gmail.com>
2026-03-26 13:02:49 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="flex h-screen bg-gray-100 items-center justify-center">
+      <h1 className="text-5xl text-black/90 font-semibold">Welcome</h1>
+    </div>
+  )
+}
+
+export default App
